07ff38fb0ef36658851ab85db2bfdd56ed4b85b5
Angel <angelloayzah2016@gmail.com>
2025-06-19 01:15:05 -0500

    Update sh and readme

--- setup.sh
+++ setup.sh
@@ -8,4 +8,7 @@ git clone git@github.com:Surveillance-IA-distributed/client-gateway.git
 echo "Clonando video-data-ms..."
 git clone git@github.com:Surveillance-IA-distributed/video-data-ms.git
 
+echo "Clonando object-detector-api..."
+git clone git@github.com:Surveillance-IA-distributed/object-detector-api.git
+
 echo "Repositorios clonados."
